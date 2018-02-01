551b427b73abc1955dac3b57e9a0eaa6ffe9bf3e
Chen Jian <lsdlab@icloud.com>
2018-02-02 01:49:33 +0800

    update

--- letter_avatar_example.py
+++ letter_avatar_example.py
@@ -1,22 +1,12 @@
-
-"""
-    Generates default avatars from a given string (such as username).
-
-    Usage:
-
-    >>> from avatar_generator import Avatar
-    >>> photo = Avatar.generate(128, "example@sysnove.fr", "PNG")
-"""
-
 import io
 from PIL import Image
 from avatar_gen.letter_avatar import LetterAvatar
 
 
 def main():
-    file_path = "/Users/Chen/PythonProjects/avatar-gen/letter_avatar.png"
     # image_byte_array = LetterAvatar.generate(128, "lsdvincent@gmail.com", "PNG")
     image_byte_array = LetterAvatar.generate(128, "陈坚", "PNG")
+    file_path = "/Users/Chen/PythonProjects/avatar-gen/letter_avatar.png"
     image = Image.open(io.BytesIO(image_byte_array))
     image.save(file_path)
 
--- pixel_avatar_example.py
+++ pixel_avatar_example.py
@@ -1,14 +1,17 @@
+import io
+from PIL import Image
 from avatar_gen.pixel_avatar import PixelAvatar
 
 
 def main():
-    # Example usage
     pixel_avatar = PixelAvatar(rows=10, columns=10)
     image_byte_array = pixel_avatar.get_image(
         string='lsdvincent@gmail.com', width=128, height=128, padding=10)
-
-    return pixel_avatar.save(
-        image_byte_array=image_byte_array, save_location='pixel_avatar.png')
+    file_path = "/Users/Chen/PythonProjects/avatar-gen/pixel_avatar.png"
+    image = Image.open(io.BytesIO(image_byte_array))
+    image.save(file_path)
+    # return pixel_avatar.save(
+        # image_byte_array=image_byte_array, save_location='pixel_avatar.png')
 
 
 if __name__ == '__main__':
